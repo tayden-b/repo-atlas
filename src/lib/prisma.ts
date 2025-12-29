b318be674939ad1495a3acd26826f6699c162947
tayden-b <tayden.barretto@hashicorp.com>
2025-12-29 15:41:57 -0600

    Fix import capitalization in prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { PrismaLibSql } from "@prisma/adapter-libsql";
 import { createClient } from "@libsql/client";
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
